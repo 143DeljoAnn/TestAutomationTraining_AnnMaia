4ca6660c58e5c42d92e8df81353f366879bb3701
143DeljoAnn <annmaria.moncy@ust.com>
2024-11-05 23:38:42 +0530

    Add files via upload

---
+++ Week11/DemRestTemplateFinal/src/main/java/com/example/DemRestTemplateFinal/DemRestTemplateFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.DemRestTemplateFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemRestTemplateFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemRestTemplateFinalApplication.class, args);
+	}
+
+}
